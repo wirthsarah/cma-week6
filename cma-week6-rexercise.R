93406dbb306f3018dc4311b02b39091f52c34473
wirthsarah <wirthsarah1@gmail.com>
2024-05-29 08:34:57 +0200

    Preparation for tasks and used data

--- cma-week6-rexercise.R
+++ cma-week6-rexercise.R
@@ -1,3 +1,10 @@
+library("readr")
+library("sf")
+
+
+wildschwein_BE <- read_delim("wildschwein_BE_2056.csv", ",") |>
+  st_as_sf(coords = c("E", "N"), crs = 2056, remove = FALSE)
+
 # Task 1 ----
 
 # Task 2 ----
